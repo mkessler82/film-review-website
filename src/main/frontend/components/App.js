132d46688b03aa04d1033472bb876586637b7c56
JR <jrpark07@gmail.com>
2021-06-28 12:11:48 -0700

    Add missing file for Nav Bar

--- src/main/frontend/components/App.js
+++ src/main/frontend/components/App.js
@@ -3,11 +3,8 @@ import { Switch, Route, Redirect, BrowserRouter } from "react-router-dom"
 import { hot } from "react-hot-loader/root";
 import "foundation-sites";
 import $ from "jquery";
-import GenresIndex from "./GenresIndex";
-import GenreShow from "./GenreShow";
-import FilmShow from "./FilmShow";
-import FilmForm from "./FilmForm";
-import "../assets/scss/main.scss"
+import "../assets/scss/main.scss";
+import NavBar from "./NavBar";
 
 const App = (props) => {
   useEffect(() => {
@@ -15,17 +12,9 @@ const App = (props) => {
   }, []);
 
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route exact path="/">
-          <Redirect to="/genres" />
-        </Route>
-        <Route exact path="/genres" component={GenresIndex} />
-        <Route exact path="/genres/:id" component={GenreShow} />
-        <Route exact path="/films/new" component={FilmForm} />
-        <Route exact path="/films/:id" component={FilmShow} />
-      </Switch>
-    </BrowserRouter>
+     <BrowserRouter>
+       <Route path="/" component={NavBar} />
+     </BrowserRouter>
   )
 };
 
---
+++ src/main/frontend/components/NavBar.js
@@ -0,0 +1,68 @@
+import React, { useState, useEffect } from "react"
+import { Switch, Route, Link, Redirect } from "react-router-dom"
+
+import GenresIndex from "./GenresIndex";
+import GenreShow from "./GenreShow";
+import FilmShow from "./FilmShow";
+import FilmForm from "./FilmForm";
+
+const NavBar = props => {
+  const [genres, setGenres] = useState([])
+
+  const fetchGenres = async () => {
+    try {
+      const response = await fetch("/api/v1/genres")
+      if(!response.ok) {
+        const errorMessage = `${response.status} (${response.statusText})`
+        const error = new Error(errorMessage)
+        throw(error)
+      }
+      const genresData = await response.json()
+      setGenres(genresData.genres.content)
+    } catch(err) {
+      console.error(`Error in fetch: ${err.message}`)
+    }
+  }
+
+  useEffect(() => {
+    fetchGenres()
+  }, [])
+
+  const genreLinks = genres.map(genre => {
+    return (
+      <li key={genre.id} className="menu-text">
+        <Link to={`/genres/${genre.id}`}>{genre.name}</Link>
+      </li>
+    )
+  })
+
+  genreLinks.unshift(
+    <li key={0} className="menu-text">
+        <Link to={`/genres`}>All Genres</Link>
+    </li>
+  )
+
+  return (
+    <div>
+     <div className="top-bar">
+      <div className="top-bar-left">
+        <ul className="menu">
+          {genreLinks}
+        </ul>
+      </div>
+     </div>
+      <Switch>
+        <Route exact path="/">
+          <Redirect to="/genres" />
+        </Route>
+        <Route exact path="/genres" component={GenresIndex} />
+        <Route exact path="/genres/:id" component={GenreShow} />
+        <Route exact path="/films/new" component={FilmForm} />
+        <Route exact path="/films/:id" component={FilmShow} />
+      </Switch>
+
+    </div>
+  )
+}
+
+export default NavBar
